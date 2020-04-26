34aff1e10138bdace6147ac5809975ed1f01ac99
Sacha O'Regan <sacha@sachaoregan.ca>
2020-04-26 13:54:54 -0700

    Style file 1

--- 1.countTerms_setup.R
+++ 1.countTerms_setup.R
@@ -52,27 +52,27 @@ Rpdf <- readPDF(control = list(text = "-layout"))
 
 # ---- Input Setup -----
 
-#Set or load terms of interest
-compoundTerms<-readr::read_csv("SearchTerms/searchterms_kg.csv")
-compoundTerms<-c(compoundTerms$dimension, compoundTerms$attribute, compoundTerms$searchterm)
-allTerms<-tolower(unique(compoundTerms))
-allTerms<-allTerms[allTerms!=""]
+# Set or load terms of interest
+compoundTerms <- readr::read_csv("SearchTerms/searchterms_kg.csv")
+compoundTerms <- c(compoundTerms$dimension, compoundTerms$attribute, compoundTerms$searchterm)
+allTerms <- tolower(unique(compoundTerms))
+allTerms <- allTerms[allTerms != ""]
 allTerms <- as.character(na.omit(allTerms))
 
 # ---- Load list of reports ----
 
 # dat<-read.xlsx("WDPA_English_Combined_2020-03-25_kg.xlsx")
-dat<-readxl::read_xlsx("WDPA_English_Combined_2020-03-25_kg.xlsx")
-names(dat) <- gsub(" ",".", names(dat))
-luCountryGroup<-read.csv("luCountryGroup.csv")
+dat <- readxl::read_xlsx("WDPA_English_Combined_2020-03-25_kg.xlsx")
+names(dat) <- gsub(" ", ".", names(dat))
+luCountryGroup <- read.csv("luCountryGroup.csv")
 
-dat2<-dat[,names(dat) %in% c("NAME","DESIG","WDPAID" ,"Parent.Nation", "Saved.File.Name", "Found", "Simplified.comments")]
-dat2$PA.Name<-paste(dat2$NAME, dat2$DESIG)
-dat2$Found<-tolower(dat2$Found)
-dat2$Simplified.comments<-tolower(dat2$Simplified.comments)
-dat2$PA.Name<-paste(dat2$NAME, dat2$DESIG)
-dat2$paper<-c(as.character(dat2$Saved.File.Name))
-dat2<-dat2[,!names(dat2) %in% c("Saved.File.Name")]
+dat2 <- dat[, names(dat) %in% c("NAME", "DESIG", "WDPAID", "Parent.Nation", "Saved.File.Name", "Found", "Simplified.comments")]
+dat2$PA.Name <- paste(dat2$NAME, dat2$DESIG)
+dat2$Found <- tolower(dat2$Found)
+dat2$Simplified.comments <- tolower(dat2$Simplified.comments)
+dat2$PA.Name <- paste(dat2$NAME, dat2$DESIG)
+dat2$paper <- c(as.character(dat2$Saved.File.Name))
+dat2 <- dat2[, !names(dat2) %in% c("Saved.File.Name")]
 
 # #optional - look at categories
 # table(dat2$Found[dat2$Simplified.comments!="duplicate"])
@@ -80,48 +80,48 @@ dat2<-dat2[,!names(dat2) %in% c("Saved.File.Name")]
 # table(dat2$Simplified.comments[dat2$Found=="no"])
 # table(dat2$Simplified.comments[dat2$Found=="yes"])
 
-#split out lines for parks with multiple papers
-dat2<-separate_rows(dat2, paper, sep = ";")
-dat2$paper<-gsub("^\\s|\\s$", "", dat2$paper)
-dat2$paper<-paste0(gsub(".pdf$","",dat2$paper), ".pdf")
-names(dat2)[names(dat2)=="Parent.Nation"]<-c("Country")
-dat2$paper[dat2$paper=="NA.pdf"]<-NA
-dat2<-merge(dat2, luCountryGroup, by="Country", all.x=T)
+# split out lines for parks with multiple papers
+dat2 <- separate_rows(dat2, paper, sep = ";")
+dat2$paper <- gsub("^\\s|\\s$", "", dat2$paper)
+dat2$paper <- paste0(gsub(".pdf$", "", dat2$paper), ".pdf")
+names(dat2)[names(dat2) == "Parent.Nation"] <- c("Country")
+dat2$paper[dat2$paper == "NA.pdf"] <- NA
+dat2 <- merge(dat2, luCountryGroup, by = "Country", all.x = T)
 
-length(unique(dat2$PA.Name)) #1449 parks
-length(unique(dat2$paper[!is.na(dat2$paper)])) #663
+length(unique(dat2$PA.Name)) # 1449 parks
+length(unique(dat2$paper[!is.na(dat2$paper)])) # 663
 
-#make lookup table for country and country grouping
-luPaper<-dat2[,!names(dat2) %in% c("Saved.File.Name", "Found", "Simplified.comments")]
-luPaper<-luPaper[!is.na(luPaper$paper),]
-luPaper$Grouping<-as.character(luPaper$Grouping)
-luPaper$Grouping[luPaper$paper=="California_MPAs.pdf"]<-"California_MPAN"
-luPaper<-luPaper[!duplicated(luPaper),]
-length(unique(luPaper$paper)) #663 unique papers
+# make lookup table for country and country grouping
+luPaper <- dat2[, !names(dat2) %in% c("Saved.File.Name", "Found", "Simplified.comments")]
+luPaper <- luPaper[!is.na(luPaper$paper), ]
+luPaper$Grouping <- as.character(luPaper$Grouping)
+luPaper$Grouping[luPaper$paper == "California_MPAs.pdf"] <- "California_MPAN"
+luPaper <- luPaper[!duplicated(luPaper), ]
+length(unique(luPaper$paper)) # 663 unique papers
 
-parkPaperSummary<-ddply(luPaper, c("paper"), summarize, nParks=length(unique(PA.Name)))
-length(unique(parkPaperSummary$paper)) #663 papers
-nrow(parkPaperSummary[parkPaperSummary$nParks>1,]) #86 papers refer to more than 1 park
+parkPaperSummary <- ddply(luPaper, c("paper"), summarize, nParks = length(unique(PA.Name)))
+length(unique(parkPaperSummary$paper)) # 663 papers
+nrow(parkPaperSummary[parkPaperSummary$nParks > 1, ]) # 86 papers refer to more than 1 park
 
-paperParkSummary<-ddply(luPaper, c("PA.Name"), summarize, nPapers=length(unique(paper)))
-length(unique(paperParkSummary$PA.Name)) #935 parks
-nrow(paperParkSummary[paperParkSummary$nPapers>1,]) #9 parks have more than 1 paper
+paperParkSummary <- ddply(luPaper, c("PA.Name"), summarize, nPapers = length(unique(paper)))
+length(unique(paperParkSummary$PA.Name)) # 935 parks
+nrow(paperParkSummary[paperParkSummary$nPapers > 1, ]) # 9 parks have more than 1 paper
 
-parkPaperByCountry<-ddply(luPaper, c("Country"),summarize, nParks=length(unique(PA.Name)), nPapers=length(unique(paper)))
-parkPaperByGrouping<-ddply(luPaper, c("Grouping"),summarize, nParks=length(unique(PA.Name)), nPapers=length(unique(paper)))
+parkPaperByCountry <- ddply(luPaper, c("Country"), summarize, nParks = length(unique(PA.Name)), nPapers = length(unique(paper)))
+parkPaperByGrouping <- ddply(luPaper, c("Grouping"), summarize, nParks = length(unique(PA.Name)), nPapers = length(unique(paper)))
 
-#Get list of PDFs and read them in
+# Get list of PDFs and read them in
 dir <- "ManagementPlans_R/"
-#list.of.pdfs<-paste0(dir,list.files(pattern="*.pdf$", recursive = TRUE))
-list.of.pdfs<-list.files(dir, pattern="*.pdf$", recursive = TRUE, full.names = TRUE)
+# list.of.pdfs<-paste0(dir,list.files(pattern="*.pdf$", recursive = TRUE))
+list.of.pdfs <- list.files(dir, pattern = "*.pdf$", recursive = TRUE, full.names = TRUE)
 
-#Compare spreadspeet to files
-inXLS_butMissing<-unique(dat2$paper)[!unique(dat2$paper) %in% gsub(dir,"",list.of.pdfs)]
-inXLS_butMissing<-inXLS_butMissing[order(inXLS_butMissing)]
+# Compare spreadspeet to files
+inXLS_butMissing <- unique(dat2$paper)[!unique(dat2$paper) %in% gsub(dir, "", list.of.pdfs)]
+inXLS_butMissing <- inXLS_butMissing[order(inXLS_butMissing)]
 
-haveFile_butNotInXLS<-gsub(dir,"",list.of.pdfs)[!gsub(dir,"",list.of.pdfs) %in% unique(dat2$paper)]
-haveFile_butNotInXLS<-haveFile_butNotInXLS[order(haveFile_butNotInXLS)]
+haveFile_butNotInXLS <- gsub(dir, "", list.of.pdfs)[!gsub(dir, "", list.of.pdfs) %in% unique(dat2$paper)]
+haveFile_butNotInXLS <- haveFile_butNotInXLS[order(haveFile_butNotInXLS)]
 
-#look at these - make sure all the papers you want to be searching are in the list/folder
+# look at these - make sure all the papers you want to be searching are in the list/folder
 inXLS_butMissing
 haveFile_butNotInXLS
