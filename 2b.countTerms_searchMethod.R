ac6687ca89816040ac5a1c636f23d7a27e8a9389
Sacha O'Regan <sacha@sachaoregan.ca>
2020-05-07 21:20:09 -0700

    Plotting

--- 2b.countTerms_searchMethod.R
+++ 2b.countTerms_searchMethod.R
@@ -87,6 +87,7 @@ terms <- furrr::future_map_dfr(corpus_selected, get_count, .s = vec,
 
 components <- components %>% rename(term = component)
 out <- left_join(components, terms) %>%
+  filter(report != "NA") %>%
   left_join(total_words) %>%
   mutate(count = ifelse(!is.na(count), count, 0)) %>%
   select(-term) %>%
@@ -110,7 +111,7 @@ tot_term_count <- components_w_meta %>%
     mean.prop = mean(prop)
     # lwr.prop = tryCatch(plogis(confint(glm(prop~1, family = binomial()))[[1]]), error = function(e) 0),
     # upr.prop = tryCatch(plogis(confint(glm(prop~1, family = binomial()))[[2]]), error = function(e) 0)
-    )
+  )
 
 # term_prop <- components_w_meta %>% mutate(root_word = stringr::str_to_sentence(root_word), dimension = stringr::str_to_sentence(dimension)) %>%
 #   group_by(dimension, root_word) %>%
@@ -138,7 +139,7 @@ ggsave("comps_v2.png", width = 5, height = 9)
 
 ggplot(filter(tot_term_count, !is.na(root_word)),
   aes(x = forcats::fct_reorder(root_word, mean.prop), y = mean.prop)) +
-  geom_segment( aes(xend = root_word, yend = 0)) +
+  geom_segment(aes(xend = root_word, yend = 0)) +
   geom_point(shape = 21, size = 2, colour = "black", aes(fill = dimension)) +
   facet_wrap(~dimension, ncol = 1, scales = "free") +
   scale_colour_manual(values = c("#31a354", "#8856a7", "#fd8d3c")) +
@@ -165,6 +166,25 @@ ggplot(filter(tot_term_count, !is.na(root_word)),
 # cowplot::plot_grid(plotlist = g)
 
 ggsave("comps_v3.png", width = 5, height = 9)
+
+components_w_meta$Grouping <-  recode(components_w_meta$Grouping, California_MPAN = "USA")
+
+components_by_region <- components_w_meta %>%
+  mutate(root_word = stringr::str_to_sentence(root_word), dimension = stringr::str_to_sentence(dimension)) %>%
+  group_by(Grouping, dimension, root_word) %>%
+  summarise(proportion = mean(count > 0))
+  #filter(term!= "NA")
+
+ggplot(components_by_region, aes(x = forcats::fct_reorder(root_word, proportion), y = proportion, fill = Grouping)) +
+  geom_col() +
+  facet_grid(dimension~Grouping, scales = "free") +
+  theme_sleek() +
+  theme(legend.position = "none", panel.spacing.x = unit(10, "pt"), plot.margin = margin(11/2, 11/2+5, 11/2, 11/2)) +
+  scale_y_continuous(breaks = c(0, 0.25, .50, 0.75, 1), labels = c("0", "0.25", "0.50", "0.75", "1.0")) +
+  labs(x = "", y = "Proportion of MPAs") + coord_flip(expand = FALSE) +
+  scale_fill_brewer(palette = "Set3")
+
+ggsave("comps_by_region.png", width = 14, height = 12)
 
 term_prop2 <- components_w_meta %>% mutate(root_word = stringr::str_to_sentence(root_word), dimension = stringr::str_to_sentence(dimension)) %>%
   group_by(dimension, root_word)
